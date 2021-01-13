29ca114b63ef690475317cd4ed2082e341b9df67
pattywagonpbj <patrick.jarrett11@gmail.com>
2021-01-12 21:40:23 -0800

    update js/app.js

--- js/app.js
+++ js/app.js
@@ -50,148 +50,169 @@ alert(`Welcome to my about me page ${userName}! \nPlease answer questions in a '
 var correctAnswers = 0;
 
 // Ask Question #1
-var answerOne = prompt('Did I go to The Evergreen State College?').toUpperCase();
-// console.log(`User normalized answer to question one is: ${answerOne}`);
-// Evaluate answer to question #1
-if (answerOne === 'YES' || answerOne === 'Y') {
-  // console.log('User has answered question #1 correctly');
-  correctAnswers++;
-  alert(`${userName} - That is correct!`);
-} else {
-  // console.log('User has answered question #1 incorrectly');
-  alert(`${userName} - That is incorrect.`);
+function questionOne(){
+  var answerOne = prompt('Did I go to The Evergreen State College?').toUpperCase();
+  // console.log(`User normalized answer to question one is: ${answerOne}`);
+  // Evaluate answer to question #1
+  if (answerOne === 'YES' || answerOne === 'Y') {
+    // console.log('User has answered question #1 correctly');
+    correctAnswers++;
+    alert(`${userName} - That is correct!`);
+  } else {
+    // console.log('User has answered question #1 incorrectly');
+    alert(`${userName} - That is incorrect.`);
+  }
 }
+questionOne();
 
 // Ask Question #2
-var answerTwo = prompt('Do I like open source projects?').toUpperCase();
-// console.log(`User normalized answer to question two is: ${answerTwo}`);
-// Evaluate answer to question #2
-if (answerTwo === 'YES' || answerTwo === 'Y') {
-  // console.log('User has answered question #2 correctly');
-  correctAnswers++;
-  alert(`${userName} - That is correct!`);
-} else {
-  // console.log('User has answered question #2 incorrectly');
-  alert(`${userName} - That is incorrect.`);
+function questionTwo(){
+  var answerTwo = prompt('Do I like open source projects?').toUpperCase();
+  // console.log(`User normalized answer to question two is: ${answerTwo}`);
+  // Evaluate answer to question #2
+  if (answerTwo === 'YES' || answerTwo === 'Y') {
+    // console.log('User has answered question #2 correctly');
+    correctAnswers++;
+    alert(`${userName} - That is correct!`);
+  } else {
+    // console.log('User has answered question #2 incorrectly');
+    alert(`${userName} - That is incorrect.`);
+  }
 }
+questionTwo();
 
 // Ask Question #3
-var answerThree = prompt('Do I test software for a living?').toUpperCase();
-// console.log(`User normalized answer to question three is: ${answerThree}`);
-// Evaluate answer to question #3
-if (answerThree === 'YES' || answerThree === 'Y') {
-  // console.log('User has answered question #3 correctly');
-  correctAnswers++;
-  alert(`${userName} - That is correct!`);
-} else {
-  // console.log('User has answered question #3 incorrectly');
-  alert(`${userName} - That is incorrect.`);
+function questionThree(){
+  var answerThree = prompt('Do I test software for a living?').toUpperCase();
+  // console.log(`User normalized answer to question three is: ${answerThree}`);
+  // Evaluate answer to question #3
+  if (answerThree === 'YES' || answerThree === 'Y') {
+    // console.log('User has answered question #3 correctly');
+    correctAnswers++;
+    alert(`${userName} - That is correct!`);
+  } else {
+    // console.log('User has answered question #3 incorrectly');
+    alert(`${userName} - That is incorrect.`);
+  }
 }
+questionThree();
 
 // Ask Question #4
-var answerFour = prompt('Does GTFS stand for \'Get Tested For Sepsis?\'').toUpperCase();
-// console.log(`User normalized answer to question four is: ${answerFour}`);
-// Evaluate answer to question #4
-if (answerFour === 'NO' || answerFour === 'N') {
-  // console.log('User has answered question #4 correctly');
-  correctAnswers++;
-  alert(`${userName} - That is correct!`);
-} else {
-  // console.log('User has answered question #4 incorrectly');
-  alert(`${userName} - That is incorrect. \nGTFS stands for General Transit Feed Specification`);
+function questionFour(){
+  var answerFour = prompt('Does GTFS stand for \'Get Tested For Sepsis?\'').toUpperCase();
+  // console.log(`User normalized answer to question four is: ${answerFour}`);
+  // Evaluate answer to question #4
+  if (answerFour === 'NO' || answerFour === 'N') {
+    // console.log('User has answered question #4 correctly');
+    correctAnswers++;
+    alert(`${userName} - That is correct!`);
+  } else {
+    // console.log('User has answered question #4 incorrectly');
+    alert(`${userName} - That is incorrect. \nGTFS stands for General Transit Feed Specification`);
+  }
 }
+questionFour();
 
 // Ask Question #5
-var answerFive = prompt(`${userName}, should developers write unit tests?`).toUpperCase();
-// console.log(`User normalized answer to question five is: ${answerFive}`);
-// Evaluate answer to question #5
-if (answerFive === 'YES' || answerFive === 'Y') {
-  // console.log('User has answered question #5 correctly.');
-  alert(`${userName} - That is correct!`);
-  correctAnswers++;
-} else {
-  // console.log('User has answered question #5 correctly');
-  alert(`${userName} - That is incorrect. Good developers write unit tests.`);
-  // window.open('https://cloud.google.com/solutions/devops/devops-tech-test-automation#ways_to_improve_continuous_testing', '_blank');
+function questionFive(){
+  var answerFive = prompt(`${userName}, should developers write unit tests?`).toUpperCase();
+  // console.log(`User normalized answer to question five is: ${answerFive}`);
+  // Evaluate answer to question #5
+  if (answerFive === 'YES' || answerFive === 'Y') {
+    // console.log('User has answered question #5 correctly.');
+    alert(`${userName} - That is correct!`);
+    correctAnswers++;
+  } else {
+    // console.log('User has answered question #5 correctly');
+    alert(`${userName} - That is incorrect. Good developers write unit tests.`);
+    // window.open('https://cloud.google.com/solutions/devops/devops-tech-test-automation#ways_to_improve_continuous_testing', '_blank');
+  }
 }
+questionFive();
 
 
 // Ask Question #6 - Guessing Game & Random Int Generator
-var randomNumber = Math.ceil(Math.random(0, 1) * 100);
-// console.log(`Random Number Generated is: ${randomNumber}`);
+function questionSix(){
+  var randomNumber = Math.ceil(Math.random(0, 1) * 100);
+  // console.log(`Random Number Generated is: ${randomNumber}`);
 
-// Array to store previous guesses - Personal Stretch Goal
-var previousGuesses = [];
+  // Array to store previous guesses - Personal Stretch Goal
+  var previousGuesses = [];
 
-// console.log(previousGuesses);
-for (let i = 0; i < 4; i++) {
-  var userGuess = +prompt(`Guess a Random Number between 0 and 100!\nNumber of guesses remaining ${4-i}`);
-  // console.log(`Random user guess is: ${randomNumber}`);
-  while (!userGuess || userGuess < 0 || userGuess > 100) {
-    userGuess = +prompt(`Converted value of: '${userGuess}' is not a number, or not in the valid range. Please try again.`);
-  }
-  // Don't count previously entered guesses against the user
-  var prevGuessUsed = false;
-  for (let j = 0; j < previousGuesses.length; j++) {
-    if (previousGuesses[j] === userGuess) {
-      alert(`You've already tried the number ${userGuess}. Please try a new number!`);
-      prevGuessUsed = true;
+  // console.log(previousGuesses);
+  for (let i = 0; i < 4; i++) {
+    var userGuess = +prompt(`Guess a Random Number between 0 and 100!\nNumber of guesses remaining ${4-i}`);
+    // console.log(`Random user guess is: ${randomNumber}`);
+    while (!userGuess || userGuess < 0 || userGuess > 100) {
+      userGuess = +prompt(`Converted value of: '${userGuess}' is not a number, or not in the valid range. Please try again.`);
+    }
+    // Don't count previously entered guesses against the user
+    var prevGuessUsed = false;
+    for (let j = 0; j < previousGuesses.length; j++) {
+      if (previousGuesses[j] === userGuess) {
+        alert(`You've already tried the number ${userGuess}. Please try a new number!`);
+        prevGuessUsed = true;
+      }
+    }
+    if (prevGuessUsed) {
+      i--;
+    // Main block - Tell user if correct or too high or too low
+    } else if (userGuess === randomNumber) {
+      correctAnswers++;
+      alert(`${userName} you guessed correctly!`);
+      break;
+    } else if (!prevGuessUsed && userGuess < randomNumber) {
+      alert(`${userName} you're guess it too low!`);
+    } else if (!prevGuessUsed && userGuess > randomNumber) {
+      alert(`${userName} you're guess it too high!`);
+    }
+
+    // Push guess to previous guesses list
+    previousGuesses.push(userGuess);
+
+    // If user runs out of guesses inform them and show the correct answer
+    if(i === 3){
+      alert(`${userName} you're out of guesses.\nThe correct number was ${randomNumber}!`);
     }
   }
-  if (prevGuessUsed) {
-    i--;
-  // Main block - Tell user if correct or too high or too low
-  } else if (userGuess === randomNumber) {
-    correctAnswers++;
-    alert(`${userName} you guessed correctly!`);
-    break;
-  } else if (!prevGuessUsed && userGuess < randomNumber) {
-    alert(`${userName} you're guess it too low!`);
-  } else if (!prevGuessUsed && userGuess > randomNumber) {
-    alert(`${userName} you're guess it too high!`);
-  }
-
-  // Push guess to previous guesses list
-  previousGuesses.push(userGuess);
-
-  // If user runs out of guesses inform them and show the correct answer
-  if(i === 3){
-    alert(`${userName} you're out of guesses.\nThe correct number was ${randomNumber}!`);
-  }
 }
+questionSix();
 
 // Ask Question #7 - A question that has multiple possible correct answers that are stored in an array.
-var answers = ['AFRICAN OR EUROPEAN?', '24 MILES PER HOUR', '24MPH', '11 METERS PER SECOND', '11MPS', '38.6KM/H', '38.6 KILOMETERS PER HOUR'];
+function questionSeven(){
+  var answers = ['AFRICAN OR EUROPEAN?', '24 MILES PER HOUR', '24MPH', '11 METERS PER SECOND', '11MPS', '38.6KM/H', '38.6 KILOMETERS PER HOUR'];
 
-// Store boolean for if answered correctly.
-var answeredCorrectly = false;
+  // Store boolean for if answered correctly.
+  var answeredCorrectly = false;
 
-for (let i = 0; i < 6; i++) {
-  // Send Prompt Every Loop
-  var airSpeedAnswer = prompt(`${userName} what is the airspeed velocity of an unladen swallow?\nThis questions is NOT in a 'Yes' / 'No' or 'Y' / 'N' format`).toUpperCase();
+  for (let i = 0; i < 6; i++) {
+    // Send Prompt Every Loop
+    var airSpeedAnswer = prompt(`${userName} what is the airspeed velocity of an unladen swallow?\nThis questions is NOT in a 'Yes' / 'No' or 'Y' / 'N' format`).toUpperCase();
 
-  // Set Answered Correctly to false
-  answeredCorrectly = false;
+    // Set Answered Correctly to false
+    answeredCorrectly = false;
 
-  // Loop through possible answers
-  for (var j = 0; j < answers.length; j++) {
-    if (airSpeedAnswer === answers[j]) {
-      answeredCorrectly = true;
+    // Loop through possible answers
+    for (var j = 0; j < answers.length; j++) {
+      if (airSpeedAnswer === answers[j]) {
+        answeredCorrectly = true;
+      }
+    }
+
+    if (answeredCorrectly) {
+      correctAnswers++;
+      alert(`${userName}, how do you know so much about swallows?`);
+    } else {
+      alert(`${userName}, that is incorrect.\nGuesses remaining ${5-i}`);
+    }
+
+    if(answeredCorrectly || i === 5) {
+      alert(`${userName}, the answers are:\n${answers}!`);
+      break;
     }
   }
-
-  if (answeredCorrectly) {
-    correctAnswers++;
-    alert(`${userName}, how do you know so much about swallows?`);
-  } else {
-    alert(`${userName}, that is incorrect.\nGuesses remaining ${5-i}`);
-  }
-
-  if(answeredCorrectly || i === 5) {
-    alert(`${userName}, the answers are:\n${answers}!`);
-    break;
-  }
 }
+questionSeven();
 
 // Return User Score in Alert
 alert(`${userName}, your score is: ${correctAnswers} / 7`);
