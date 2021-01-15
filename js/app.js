7bd199975524b3b08ef8cf0c1feec9801bba254d
MuckT <mcgtom10@gmail.com>
2021-01-15 10:05:28 -0800

    feat: full send on event listeners

--- js/app.js
+++ js/app.js
@@ -19,6 +19,11 @@ var setViewHeight = function () {
 // Add our event listeners
 window.addEventListener('load', setViewHeight);
 window.addEventListener('resize', setViewHeight);
+window.addEventListener('orientationchange', setViewHeight);
+document.addEventListener('webkitfullscreenchange', setViewHeight);
+document.addEventListener('mozfullscreenchange', setViewHeight);
+document.addEventListener('fullscreenchange', setViewHeight);
+document.addEventListener('msfullscreenchange', setViewHeight);
 
 /*
 // Ask The User their name - Display it in an alert - Force the user to enter a name
